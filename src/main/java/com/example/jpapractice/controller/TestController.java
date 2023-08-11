4a11c558c050ca2d2fb63f4b4c708242faad2c0d
sm <mina0ff@t-win.kr>
2023-08-11 14:24:10 +0900

    test 컨트롤러 좀 바꿈

--- src/main/java/com/example/jpapractice/controller/TestController.java
+++ src/main/java/com/example/jpapractice/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.example.jpapractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+    @GetMapping("")
+    public String test(){
+        return "test123123123121";
+    }
 }
