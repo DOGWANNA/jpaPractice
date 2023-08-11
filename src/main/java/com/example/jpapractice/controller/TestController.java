715c430bb0e5c36086a9ed84662428edc5ee2576
sm <mina0ff@t-win.kr>
2023-08-11 14:04:52 +0900

    test

--- src/main/java/com/example/jpapractice/controller/TestController.java
+++ src/main/java/com/example/jpapractice/controller/TestController.java
@@ -1,7 +1,13 @@
 package com.example.jpapractice.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
+
+    @GetMapping("/")
+    public String test(){
+        return "test123";
+    }
 }
