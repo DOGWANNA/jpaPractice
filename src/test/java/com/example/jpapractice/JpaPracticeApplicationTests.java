5baba3ef94abb0def17b71658145ac5a07363429
DOKWAN NA <9401ndk@naver.com>
2023-09-25 16:04:37 +0900

    feat: 세션 저장소 설정 추가

--- src/test/java/com/example/jpapractice/JpaPracticeApplicationTests.java
+++ src/test/java/com/example/jpapractice/JpaPracticeApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = UserService.class)
 public class JpaPracticeApplicationTests {
-////
+/////
 //    @Autowired
 //    private MockMvc mvc;
 //    @Test
